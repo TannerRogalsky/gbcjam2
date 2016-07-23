9870952b140a41cfeab48cc0573d31902f6168a0
Tanner Rogalsky <tanner@tannerrogalsky.com>
2016-07-23 14:41:18 -0400

    don’t get stuck in game over

--- states/game/game_over.lua
+++ states/game/game_over.lua
@@ -3,6 +3,14 @@ local Over = Game:addState('Over')
 function Over:enteredState()
 end
 
+function Over:mousereleased()
+  self:gotoState('Menu')
+end
+
+function Over:keyreleased()
+  self:gotoState('Menu')
+end
+
 function Over:exitedState()
 end
 
